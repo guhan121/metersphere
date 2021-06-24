72182b91a42aa9c14636c9afb3a8c264a8da734f
chenyansong <chenyansong@joyy.sg>
2021-06-24 20:43:36 +0800

    应用启动时加载hosts文件

---
+++ backend/src/main/java/io/metersphere/config/NameServiceInitRunner.java
@@ -0,0 +1,25 @@
+package io.metersphere.config;
+
+import io.metersphere.commons.utils.LogUtil;
+import io.metersphere.nameservice.CustomNameService;
+import io.metersphere.service.SystemParameterService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+@Component
+public class NameServiceInitRunner implements ApplicationRunner {
+
+    @Resource
+    private SystemParameterService systemParameterService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Map<String, String> mapping = systemParameterService.getHostMap();
+        CustomNameService.load(mapping);
+        LogUtil.info("Load hosts mapping success");
+    }
+}
